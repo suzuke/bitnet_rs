8be3de34e96a875aaae76549e37742babf9d6448
suzuke <suzuke789@gmail.com>
2024-03-21 14:35:03 +0800

    Refactored bitlinear and bitlinear1_58 to resolve the conflict issue when using config.bias and the bias option simultaneously

--- src/bitlinear.rs
+++ src/bitlinear.rs
@@ -27,17 +27,19 @@ pub struct BitLinear {
     weight: Tensor,
     bias: Option<Tensor>,
     q_b: f64,
-    config: BitLinearConfig,
+    eps: f64,
+    use_before_nonlinear: bool,
 }
 
 impl BitLinear {
-    pub fn new(weight: Tensor, bias: Option<Tensor>, config: BitLinearConfig) -> Self {
-        let q_b = 2.0_f64.powi(config.bit_width as i32 - 1);
+    pub fn new(weight: Tensor, bias: Option<Tensor>, bit_width: usize, eps: f64, use_before_nonlinear: bool) -> Self {
+        let q_b = 2.0_f64.powi(bit_width as i32 - 1);
         Self {
             weight,
             bias,
             q_b,
-            config,
+            eps,
+            use_before_nonlinear,
         }
     }
 
@@ -58,14 +60,14 @@ impl BitLinear {
     fn quantize(&self, x: &Tensor) -> Result<(Tensor, Tensor)> {
         let gamma = gamma(x)?;
         let quantized_x = (x.broadcast_div(&gamma)? * self.q_b)?
-            .clamp(-self.q_b + self.config.eps, self.q_b - self.config.eps)?;
+            .clamp(-self.q_b + self.eps, self.q_b - self.eps)?;
         Ok((quantized_x, gamma))
     }
     fn quantize_nonlinear(&self, x: &Tensor) -> Result<(Tensor, Tensor)> {
         let gamma = gamma(x)?;
         let eta = min_all(x)?;
         let quantized_x = (x.broadcast_sub(&eta)?.broadcast_div(&gamma)? * self.q_b)?
-            .clamp(self.config.eps, self.q_b - self.config.eps)?;
+            .clamp(self.eps, self.q_b - self.eps)?;
         Ok((quantized_x, gamma))
     }
     fn dequantize(&self, x: &Tensor, gamma: &Tensor) -> Result<Tensor> {
@@ -77,11 +79,11 @@ impl BitLinear {
 
 impl Module for BitLinear {
     fn forward(&self, x: &Tensor) -> Result<Tensor> {
-        let x = sub_ln(x, self.config.eps)?;
+        let x = sub_ln(x, self.eps)?;
 
         let binarized_weight = self.binarize_weight()?;
 
-        let (quantized_x, gamma) = match self.config.use_before_nonlinear {
+        let (quantized_x, gamma) = match self.use_before_nonlinear {
             true => self.quantize_nonlinear(&x),
             false => self.quantize(&x),
         }?;
@@ -107,10 +109,9 @@ pub fn bitlinear<C: Into<BitLinearConfig>>(
         lo: -bound,
         up: bound,
     };
-    let bias = config
-        .bias
+    let bias = config.bias
         .then(|| vb.get_with_hints(out_dim, "bias", init_bs).unwrap());
-    Ok(BitLinear::new(weight, bias, config))
+    Ok(BitLinear::new(weight, bias, config.bit_width, config.eps, config.use_before_nonlinear))
 }
 
 #[cfg(test)]
@@ -119,18 +120,19 @@ mod tests {
     use candle_core::{DType, Device, Tensor};
 
     const DEVICE : &Device = &Device::Cpu;
+    const DTYPE : DType = DType::F32;
 
     #[test]
     fn test_bitlinear() {
         let batch_dim = 1;
         let in_dim = 4;
         let out_dim = 3;
-        let weight = Tensor::ones((out_dim, in_dim), DType::F32, DEVICE).unwrap();
-        let bias = Tensor::zeros(out_dim, DType::F32, DEVICE).unwrap();
+        let weight = Tensor::ones((out_dim, in_dim), DTYPE, DEVICE).unwrap();
+        let bias = Tensor::zeros(out_dim, DTYPE, DEVICE).unwrap();
         let config = BitLinearConfig::default();
-        let bitlinear = BitLinear::new(weight.clone(), Some(bias), config);
+        let bitlinear = BitLinear::new(weight.clone(), Some(bias), config.bit_width, config.eps, config.use_before_nonlinear);
 
-        let input = Tensor::ones((batch_dim, in_dim), DType::F32, DEVICE).unwrap();
+        let input = Tensor::ones((batch_dim, in_dim), DTYPE, DEVICE).unwrap();
         let output = bitlinear.forward(&input).unwrap();
 
         assert_eq!(bitlinear.weight().to_vec2::<f32>().unwrap(), weight.to_vec2::<f32>().unwrap());
@@ -139,11 +141,12 @@ mod tests {
 
     #[test]
     fn binarize_weight() {
-        let weight = Tensor::from_vec(vec![-0.1f32, 1.9f32, 0.0f32, 0.5f32], (2, 2), DEVICE).unwrap();
+        let weight = Tensor::from_vec(vec![-0.1f32, 1.9, 0.0, 0.5], (2, 2), DEVICE).unwrap();
+        //alpha = (-0.1+1.9+0.0+0.5)/4 = 0.575, [[-0.1-0.575, 1.9-0.575], [0.0-0.575, 0.5-0.575]] = [[-0.675,  1.325], [-0.575, -0.075]]
         let config = BitLinearConfig::default();
-        let bitlinear = BitLinear::new(weight.clone(), None, config);
+        let bitlinear = BitLinear::new(weight.clone(), None, config.bit_width, config.eps, config.use_before_nonlinear);
         let binarized_weight = bitlinear.binarize_weight().unwrap();
-
+        //sign([[-0.675,  1.325], [-0.575, -0.075]]) = [[-1, 1], [-1, -1]]
         assert_eq!(binarized_weight.to_vec2::<f32>().unwrap(), vec![vec![-1f32, 1f32], vec![-1f32, -1f32]]);
     }
 }
--- src/bitlinear1_58.rs
+++ src/bitlinear1_58.rs
@@ -25,17 +25,17 @@ pub struct BitLinear1_58 {
     weight: Tensor,
     bias: Option<Tensor>,
     q_b: f64,
-    config: BitLinear1_58Config,
+    eps: f64,
 }
 
 impl BitLinear1_58 {
-    pub fn new(weight: Tensor, bias: Option<Tensor>, config: BitLinear1_58Config) -> Self {
-        let q_b = 2.0_f64.powi(config.bit_width as i32 - 1);
+    pub fn new(weight: Tensor, bias: Option<Tensor>, bit_width: usize, eps: f64) -> Self {
+        let q_b = 2.0_f64.powi(bit_width as i32 - 1);
         Self {
             weight,
             bias,
             q_b,
-            config,
+            eps,
         }
     }
 
@@ -49,14 +49,14 @@ impl BitLinear1_58 {
 
     fn binarize_weight(&self) -> Result<Tensor> {
         let gamma = gamma1_58(&self.weight)?;
-        let binarized_weight = round_clip_ste(&self.weight.broadcast_div(&(&gamma + self.config.eps)?)?, -1.0, 1.0);
+        let binarized_weight = round_clip_ste(&self.weight.broadcast_div(&(&gamma + self.eps)?)?, -1.0, 1.0);
         binarized_weight
     }
 
     fn quantize(&self, x: &Tensor) -> Result<(Tensor, Tensor)> {
         let gamma = gamma(x)?;
         let quantized_x = (x.broadcast_div(&gamma)? * self.q_b)?
-            .clamp(-self.q_b + self.config.eps, self.q_b - self.config.eps)?;
+            .clamp(-self.q_b + self.eps, self.q_b - self.eps)?;
         Ok((quantized_x, gamma))
     }
 
@@ -69,7 +69,7 @@ impl BitLinear1_58 {
 
 impl Module for BitLinear1_58 {
     fn forward(&self, x: &Tensor) -> Result<Tensor> {
-        let x = sub_ln(x, self.config.eps)?;
+        let x = sub_ln(x, self.eps)?;
 
         let binarized_weight = self.binarize_weight()?;
 
@@ -99,7 +99,7 @@ pub fn bitlinear1_58<C: Into<BitLinear1_58Config>>(
     let bias = config
         .bias
         .then(|| vb.get_with_hints(out_dim, "bias", init_bs).unwrap());
-    Ok(BitLinear1_58::new(weight, bias, config))
+    Ok(BitLinear1_58::new(weight, bias, config.bit_width, config.eps))
 }
 
 
@@ -109,18 +109,19 @@ mod tests {
     use candle_core::{DType, Device, Tensor};
 
     const DEVICE : &Device = &Device::Cpu;
+    const DTYPE : DType = DType::F32;
 
     #[test]
     fn test_bitlinear1_58() {
         let batch_dim = 1;
         let in_dim = 4;
         let out_dim = 3;
-        let weight = Tensor::ones((out_dim, in_dim), DType::F32, DEVICE).unwrap();
-        let bias = Tensor::zeros(out_dim, DType::F32, DEVICE).unwrap();
+        let weight = Tensor::ones((out_dim, in_dim), DTYPE, DEVICE).unwrap();
+        let bias = Tensor::zeros(out_dim, DTYPE, DEVICE).unwrap();
         let config = BitLinear1_58Config::default();
-        let bitlinear = BitLinear1_58::new(weight.clone(), Some(bias), config);
+        let bitlinear = BitLinear1_58::new(weight.clone(), Some(bias), config.bit_width, config.eps);
 
-        let input = Tensor::ones((batch_dim, in_dim), DType::F32, DEVICE).unwrap();
+        let input = Tensor::ones((batch_dim, in_dim), DTYPE, DEVICE).unwrap();
         let output = bitlinear.forward(&input).unwrap();
 
         assert_eq!(bitlinear.weight().to_vec2::<f32>().unwrap(), weight.to_vec2::<f32>().unwrap());
@@ -129,11 +130,13 @@ mod tests {
 
     #[test]
     fn binarize_weight() {
-        let weight = Tensor::from_vec(vec![-0.1f32, 1.9f32, 0.0f32, 0.5f32], (2, 2), DEVICE).unwrap();
+        let weight = Tensor::from_vec(vec![-0.1f32, 1.9, 0.0, 0.5], (2, 2), DEVICE).unwrap();
+        // (0.1+1.9+0.0+0.5)/4 = 0.625, [[-0.1, 1.9], [0.0, 0.5]]/(0.625+1e-5) = [[-0.15999744, 3.03995136],[ 0., 0.7999872 ]]
         let config = BitLinear1_58Config::default();
-        let bitlinear = BitLinear1_58::new(weight.clone(), None, config);
+        let bitlinear = BitLinear1_58::new(weight.clone(), None, config.bit_width, config.eps);
         let binarized_weight = bitlinear.binarize_weight().unwrap();
-
+        //Round([[-0.15999744, 3.03995136],[ 0., 0.7999872 ]]) = [[0.,  3.],[ 0.,  1.]]
+        //Clip([0.,  3.],[ 0.,  1.]) = [[0.,  1.],[0.,  1.]]
         assert_eq!(binarized_weight.to_vec2::<f32>().unwrap(), vec![vec![0f32, 1f32], vec![0f32, 1f32]]);
     }
 }
